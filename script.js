109068b1f979d42efd32aef9cbd9b335bddb592b
Léo Cheikh-Boukal <leo.ultimate34@gmail.com>
2026-02-08 11:04:34 -0500

    Finish Borealis page

--- script.js
+++ script.js
@@ -2,35 +2,35 @@
     {
         title: "Mini Physics Engine",
         description: "Development of a C++ physics engine using OpenFramework: collision handling and dynamic simulation within an R&D course at UQAC.",
-        image: "Assets/images/Moteur.png", // Plus de dossier, juste le nom
+        image: "Assets/Moteur.png", // Plus de dossier, juste le nom
         tags: ["C++", "OpenFramework", "Physics", "R&D"],
         link: "https://github.com/Leptar/AYLMoteurPhysique"
     },
     {
         title: "One Last Stake",
         description: "Game created on Unreal Engine 5 in 48h for the Wonderjam UQAC Fall 2025. Available on Itch.io.",
-        image: "Assets/images/OnelastStake.png",
+        image: "Assets/OnelastStake.png",
         tags: ["Unreal Engine 5", "Game Jam", "48h", "C++"],
         link: "https://eyrone.itch.io/one-last-stake"
     },
     {
         title: "Borealis",
         description: "Creation of a complete game prototype (Game Design & Code) as part of a university course at UQAC.",
-        image: "Assets/images/Borealis.png",
+        image: "Assets/Borealis.png",
         tags: ["UQAC", "Prototype", "Game Design"],
-        link: "https://tomdouaud.itch.io/borealis"
+        link: "projects/Borealis.html"
     },
     {
         title: "Violence is always the Answer",
         description: "Game created in 72h for the Mini Jam 167 on Godot. Ranked and playable online.",
-        image: "Assets/images/VIAS.png",
+        image: "Assets/VIAS.png",
         tags: ["Godot", "Game Jam", "GDScript", "72h"],
         link: "https://tomdouaud.itch.io/violence-is-always-the-anwser"
     },
     {
         title: "Corpjackers",
         description: "Game Prototype project, for UQAC classes. A mix between Guess who, and The Matriarch",
-        image: "Assets/images/Corpjackers.png",
+        image: "Assets/Corpjackers.png",
         tags: ["UE5", "Project", "Game Network", "Teamwork"],
         link: "https://tomdouaud.itch.io/violence-is-always-the-anwser"
     }
@@ -48,7 +48,7 @@ projects.forEach(project => {
     const tagsHtml = project.tags.map(tag => `<span>${tag}</span>`).join('');
 
     const linkHtml = project.link && project.link !== "#"
-        ? `<a href="${project.link}" target="_blank" class="project-link">View Project →</a>`
+        ? `<a href="${project.link}" target="_self" class="project-link">View Project →</a>`
         : '';
 
     card.innerHTML = `
